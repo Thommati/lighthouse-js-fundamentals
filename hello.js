ff97d589328a0b4e76ebdb285cfc48129956f7d4
Matthew Thompson <supra__charged@hotmail.com>
2021-03-23 19:58:07 +0000

    hello - return and console.log

--- hello.js
+++ hello.js
@@ -5,3 +5,17 @@ const sayHello = function(name) {
 sayHello('Caliban');
 sayHello('Miranda');
 sayHello('Ferdinand');
+
+
+// return and console.log
+
+const sayHelloToConsole = function(name) {
+  console.log('Hello, ' + name);
+}
+sayHelloToConsole('John');
+
+const returnSayHello = function(name) {
+  return 'Hello, ' + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
